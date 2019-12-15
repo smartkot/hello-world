455c68a3fd7915fb521f2f4a5034dd49f3dbe0a4
Denis Borisov <webtrudnn@gmail.com>
2019-12-15 14:39:28 +0300

    Commit.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+
+        String hello = "Hello";
+        String world = "wold";
+        System.out.println(hello + " " + world + "!");
+    }
+}
